3bb151367032a71d166811ff52f573cf226b3b8b
Yogesh Kalbhore <yogeshkalbhore@gmail.com>
2014-06-16 18:13:50 +0530

    Changes !!

---
+++ WebServices/src/com/hashedin/data/WriteJsonFile.java
@@ -0,0 +1,47 @@
+package com.hashedin.data;
+
+
+
+	import java.io.FileWriter;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+	 
+	public class WriteJsonFile  {
+	    
+		
+		@SuppressWarnings("unchecked")
+		public static void main(String[] args) {
+	 
+		JSONObject obj = new JSONObject();
+		obj.put("name", "Yogesh");
+		obj.put("id", new Integer(50));
+	    obj.put("Location", "Bangalore");
+	    
+	    
+		JSONArray list = new JSONArray();
+		list.add("msg 1");
+		list.add("msg 2");
+		list.add("msg 3");
+	 
+		obj.put("messages", list);
+	 
+		try {
+	 
+			FileWriter file = new FileWriter("./data/data.json");
+			file.write(obj.toJSONString());
+			file.flush();
+			file.close();
+	 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	 
+		System.out.print(obj);
+	 
+	     }
+	 
+	}
+
+
